8e3fca58d962717efa747d7fcc6b8c21ab09c5a1
MakarCodes <jarek.strupek@wp.pl>
2020-06-14 23:12:59 +0200

    Add event logic fot dots

--- slider.js
+++ slider.js
@@ -59,7 +59,20 @@ prevButton.addEventListener('click', e => {
     hideShowArrows(prevButton, nextButton, nextIndex);
 });
 
+dotsNav.addEventListener('click', e => {
+    const targetDot = e.target.closest('button');
 
+    if(!targetDot) return;
+
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+    hideShowArrows(slides, prevButton, nextButton, targetIndex);
+});
 
 
 
